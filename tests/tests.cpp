07971add6f5c3aec10499ddc6527b227928d6e96
al2013 <alex.bogdanov2012@gmail.com>
2013-03-04 13:46:44 +0900

    Tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-
-int main()
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Hello
+#include <boost/test/unit_test.hpp>
+ 
+int add(int i, int j)
 {
-	std::cout<<"hello! I'm tests-program"<<"\n";
+    return i + j;
+}
+ 
+BOOST_AUTO_TEST_CASE(universeInOrder)
+{
+    BOOST_CHECK(add(2, 2) == 4);
+}
+
+BOOST_AUTO_TEST_CASE(sum1)
+{
+    BOOST_CHECK(5*5 == 10+15);
+}
+
+BOOST_AUTO_TEST_CASE(sum2)
+{
+    BOOST_CHECK(4.4*6 - 24 <= 0);
 }
