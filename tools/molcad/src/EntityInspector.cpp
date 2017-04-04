f925f001d8d47fc55cbbb59e1cee7534c576dc52
nvpopov <brodiaga38@gmail.com>
2017-04-04 21:12:18 +0800

    Travis and docs in progress

--- tools/molcad/src/EntityInspector.cpp
+++
@@ -1,8 +0,0 @@
-#include "EntityInspector.hpp"
-
-using namespace molcad;
-
-EntityInspector::EntityInspector(QWidget *parent):QTreeWidget(parent)
-{
-	header()->hide();
-}
--- tools/molcad/src/EntityMolecule.cpp
+++
@@ -1,1 +0,0 @@
-#include "EntityMolecule.hpp"
