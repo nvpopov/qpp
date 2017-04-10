425b34ddddabf087a407e321221158be75cbfb73
Andrey Mysovsky <mysovsky@gmail.com>
2017-04-10 15:04:45 +0800

    General symmetry in C++ & Python

---
+++ examples/symm/symm_oh.cpp
@@ -0,0 +1,53 @@
+#include <symm/symm.hpp>
+#include <symm/group_theory.hpp>
+#include <iostream>
+
+using namespace qpp;
+
+void prnmtr(const matrix3d<double> & M)
+{
+  for (int i=0; i<3; i++)
+    {
+      for (int j=0; j<3; j++)
+	std::cout << boost::format("%10.6f ") % M(i,j);
+      std::cout << std::endl;
+    }
+}
+
+int main()
+{
+  matrix3d<double> C4x = RotMtrx({1,0,0},pi/2);
+  matrix3d<double> C4y = RotMtrx({0,1,0},pi/2);
+  matrix3d<double> I(-1);
+
+  generated_group<matrix3d<double> > OH;
+
+  OH.add(C4x);
+  OH.add(C4y);
+  OH.add(I);
+  
+  for (int i=0; i<OH.size(); i++)
+    {
+      std::cout << i << std::endl;
+      prnmtr(OH[i]);
+    }
+  std::cout << OH.size() << std::endl;
+
+  int i1 = OH.index(C4x),
+    i2 = OH.index(C4y);
+
+  std::cout <<"C4x index is " << i1 << " C4y index is " << i2 << std::endl;
+
+  group_analyzer< matrix3d<double>, decltype(OH) > AOH(OH);
+
+  std::cout << "Abelian subgroup of C4x: ";
+  for (int g : AOH.abelian_sub(i1))
+    std::cout << g << " ";
+  std::cout << "\n";
+
+  std::cout << " Two subgroup product: ";
+  for (int g : AOH.mul_subs(AOH.abelian_sub(i1),AOH.abelian_sub(i2)))
+    std::cout << g << " ";
+  std::cout << "\n";
+  
+}
