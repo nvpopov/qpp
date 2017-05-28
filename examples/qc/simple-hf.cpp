822897167bff9eae0a27a22b80a4a94e64207835
nvpopov <brodiaga38@gmail.com>
2017-05-28 16:10:59 +0800

    travis update for libint testing

---
+++ examples/qc/simple-hf.cpp
@@ -0,0 +1,10 @@
+#include <fmt/format.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Eigen/Dense>
+#include <libint2.hpp>
+
+int main(){
+    std::cout<<fmt::format("Test format string is {} and {}\n", "dsds", "dsdssdsd");
+}
