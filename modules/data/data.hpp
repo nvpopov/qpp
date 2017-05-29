f6dd038136e59b48f0e9bb44444db6cd07ae8686
Andrey Mysovsky <mysovsky@gmail.com>
2017-05-29 23:22:05 +0800

    cdc_crystal

--- modules/data/data.hpp
+++ modules/data/data.hpp
@@ -2,6 +2,7 @@
 #define _QPPDATA_H
 
 #include <vector>
+#include <typeinfo>
 #include <data/types.hpp>
 
 namespace qpp{
@@ -11,7 +12,140 @@ namespace qpp{
     void *ptr;
     bool del;
   public:
-    datum()
+
+    datum(const STRING & s)
+    {
+      del = true;
+      tp = type_string;
+      ptr = new STRING(s);
+      //std::cout << "string datum created\n";
+    }
+
+    datum(const char * s)
+    {
+      del = true;
+      tp = type_string;
+      ptr = new STRING(s);
+      //std::cout << "string datum created\n";
+    }
+
+    datum(int i)
+    {
+      del = true;
+      tp = type_int;
+      ptr = new int(i);
+      //std::cout << "int datum created\n";
+    }
+
+    datum(float f)
+    {
+      del = true;
+      tp = type_float;
+      ptr = new float(f);
+    }
+
+    datum(double d)
+    {
+      del = true;
+      tp = type_double;
+      ptr = new double(d);
+    }
+
+    datum(bool b)
+    {
+      del = true;
+      tp = type_bool;
+      ptr = new bool(b);
+    }
+
+    datum(STRING *s)
+    {
+      del = false;
+      tp = type_string;
+      ptr = s;
+    }
+
+    datum(int *i)
+    {
+      del = false;
+      tp = type_int;
+      ptr = i;
+    }
+
+    datum(float *f)
+    {
+      del = false;
+      tp = type_float;
+      ptr = f;
+    }
+
+    datum(double *d)
+    {
+      del = false;
+      tp = type_double;
+      ptr = d;
+    }
+
+    datum(bool *b)
+    {
+      del = false;
+      tp = type_bool;
+      ptr = b;
+    }
+
+
+    template<class T>
+    T substance() const
+    {
+      bool type_error;
+      switch(tp)
+	{
+	case type_int:    type_error = typeid(T)!=typeid(int);    break;
+	case type_float:  type_error = typeid(T)!=typeid(float);  break;
+	case type_double: type_error = typeid(T)!=typeid(double); break;
+	case type_bool:   type_error = typeid(T)!=typeid(bool);   break;
+	case type_string: type_error = typeid(T)!=typeid(STRING); break;
+	default: type_error = true;
+	}
+      if (type_error)
+	{}//Make error
+
+      std::cout << "extract " << typeid(T).name() << " error= " << type_error << "\n";
+      return *((T*)ptr);      
+    }
+
+    template<class T>
+    T & substance()
+    {
+      bool type_error;
+      switch(tp)
+	{
+	case type_int:    type_error = typeid(T)!=typeid(int);    break;
+	case type_float:  type_error = typeid(T)!=typeid(float);  break;
+	case type_double: type_error = typeid(T)!=typeid(double); break;
+	case type_bool:   type_error = typeid(T)!=typeid(bool);   break;
+	case type_string: type_error = typeid(T)!=typeid(STRING); break;
+	default: type_error = true;
+	}
+      if (type_error)
+	{}//Make error
+      return *((T*)ptr);      
+    }
+
+    basic_types type(){return tp;}
+
+    ~datum()
+    {
+      if (del)
+      switch(tp)
+	{
+	case type_int:    delete (int*)ptr;     break;
+	case type_float:  delete (float*)ptr;   break;
+	case type_double: delete (double*)ptr;  break; 
+	case type_bool:   delete (bool*)ptr;    break; 
+	case type_string: delete (STRING*)ptr;  break;
+	}
+    }
 
   };
 
--- modules/python/qppython.cpp
+++ modules/python/qppython.cpp
@@ -1,6 +1,7 @@
-#define PY_EXPORT
+//#define PY_EXPORT
 
 #include <python/qppcpp.hpp>
+/*
 #include <boost/python.hpp>
 #include <boost/python/list.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
@@ -8,35 +9,7 @@
 
 using namespace boost::python;
 namespace sn = boost::python::self_ns;
-
-
-namespace qpp{
-
-  void IndexError(const char * msg)
-  {
-    PyErr_SetString(PyExc_IndexError, msg);
-    throw_error_already_set();
-  }
-
-  void TypeError(const char * msg)
-  {
-    PyErr_SetString(PyExc_TypeError, msg);
-    throw_error_already_set();
-  }
-
-  void KeyError(const char * msg)
-  {
-    PyErr_SetString(PyExc_KeyError, msg);
-    throw_error_already_set();
-  }
-
-  void StopIter()
-  {
-    PyErr_SetString(PyExc_StopIteration, "");
-    throw_error_already_set();
-  }
-
-};
+*/
 
 BOOST_PYTHON_MODULE(qpp_cpp)
 {  
