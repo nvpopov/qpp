7b20fee05285f111d6e1832496400707e97cb043
Andrey Mysovsky <mysovsky@gmail.com>
2017-10-27 14:28:33 +0800

    ioxyz.py

--- modules/python/ioxyz.py
+++ modules/python/ioxyz.py
@@ -1,4 +1,15 @@
-import qpp
+from qpp import *
+
+def read_xyz(geom,filename):
+    f = open(filename,"r")
+    nat = int(f.readline().split()[0])
+    s=f.readline().split()
+    if geom.dim == 3:
+        if len(s) == 9:
+            for i in range(3):
+                for j in range(3):
+                    geom.cell[i,j] = float(s[3*i+j])
+    
 
 def write_xyz(geom,filename):
     
