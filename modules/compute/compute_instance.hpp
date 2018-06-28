f3d0b5484068ed257c7871bd3853785e67c2ac7d
nvpopov <brodiaga38@gmail.com>
2018-06-28 23:59:03 +0800

    initial compute proporsal

--- modules/compute/compute_instance.hpp
+++ modules/compute/compute_instance.hpp
@@ -1,14 +1,24 @@
 #ifndef _QPP_COMPUTE_INSTANCE
 #define _QPP_COMPUTE_INSTANCE
 #include <geom/geom.hpp>
+#include <compute/compute_driver.hpp>
 
 namespace qpp{
 
-  template<typename REAL, typename CELL>
+  template<typename REAL>
   class compute_instance {
   public:
-    qpp::geometry<REAL, CELL> geom;
-    virtual compute_instance() = 0;
+    compute_instance(){
+
+    }
+
+    REAL energy(){
+      return 0.0;
+    }
+
+    gradient(std::vector<qpp::vector3d<REAL> > &grad){
+
+    }
   };
 
 }
