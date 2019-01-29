ca54fc21f30230dcb5716375f1dd84898f357fff
Andrey Mysovsky <mysovsky@gmail.com>
2019-01-29 16:51:37 +0800

    symmetry fixed

---
+++ examples/symm/find_point_group.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <symm/point_groups.hpp>
+
+using namespace qpp;
+
+int main(){
+
+  // Create geometry
+  geometry<float> g;
+  g.add("C", 0., 0., -.7);
+  g.add("C", 0., 0.,  .7);
+  g.add("H", .8, 0.,  .95);
+  g.add("H",-.8, 0., -.95);
+  g.add("H",-.4, .7,  .95);
+  g.add("H",-.4,-.7,  .95);
+  g.add("H", .4, .7, -.95);
+  g.add("H", .4,-.7, -.95);
+
+  array_group<matrix3<float> > G;
+
+  
+  float eps = .1;
+  find_point_symm(G,g,eps);
+}
+
