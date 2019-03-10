8aed62992b0288e1583682dfb490ad7a444850bb
nvpopov <brodiaga38@gmail.com>
2019-03-10 17:38:23 +0800

    fix: caseicomp, _sprintf for msvc2017

--- modules/data/case_icomp.hpp
+++ modules/data/case_icomp.hpp
@@ -5,17 +5,22 @@
 #include <strings.h>
 #endif
 
+#ifdef _MSC_VER
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#endif
+
 #include <map>
 #include <string>
 
 namespace qpp {
 
-    template<typename T>
-    struct case_icomp {
-            bool operator()(const T& a, const T& b) const noexcept {
-                return ::strcasecmp(a.c_str(), b.c_str()) < 0;
-            }
-    };
+  template<typename T>
+  struct case_icomp {
+      bool operator()(const T& a, const T& b) const noexcept {
+        return ::strcasecmp(a.c_str(), b.c_str()) < 0;
+      }
+  };
 
 }
 
