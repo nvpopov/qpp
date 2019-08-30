529beb815332bb4cb74c830313691425d7997805
nvpopov <brodiaga38@gmail.com>
2019-08-31 00:20:47 +0800

    fix(io): fixed frac coord output for adme ewald uc

--- modules/io/adme.hpp
+++ modules/io/adme.hpp
@@ -33,13 +33,18 @@ namespace qpp {
             fmt::print(out, "{:15.8f} {:15.8f} {:15.8f} 6 \n",
                        geom.cell.v[i][0], geom.cell.v[i][1], geom.cell.v[i][2]);
 
-        for (size_t i = 0; i < geom.nat(); i++)
-            fmt::print(out,
-                       "{:15.8f} {:15.8f} {:15.8f} {:15.8f} {}\n",
-                       geom.pos(i)[0], geom.pos(i)[1], geom.pos(i)[2],
-                       xg ? xg->charge(i) : 0,
-                       geom.atom(i)
-                    );
+        for (size_t i = 0; i < geom.nat(); i++) {
+
+                vector3<REAL> frac_c = geom.cell.cart2frac(geom.pos(i));
+
+                fmt::print(out,
+                           "{:15.8f} {:15.8f} {:15.8f} {:15.8f} {}\n",
+                           frac_c[0], frac_c[1], frac_c[2],
+                           xg ? xg->charge(i) : 0,
+                           geom.atom(i)
+                        );
+
+            }
     }
 
 }
